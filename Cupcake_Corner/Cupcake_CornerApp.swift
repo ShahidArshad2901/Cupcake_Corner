f569c86aee0009a4248dae47b76f44d297f290bd
Shahid Arshad <shahid.arshad@devsinc.com>
2023-03-01 07:42:52 -0500

    Initial Commit

---
+++ Cupcake_Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake_Corner
+//
+//  Created by Dev on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
