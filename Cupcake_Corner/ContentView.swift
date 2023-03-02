61dd22aaa7eba13fb40388af371b50c09533d909
Shahid Arshad <shahid.arshad@devsinc.com>
2023-03-02 06:34:11 -0500

    Loading an image from a remote server

--- Cupcake_Corner/ContentView.swift
+++ Cupcake_Corner/ContentView.swift
@@ -7,50 +7,44 @@
 
 import SwiftUI
 
-
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
-// async
-
 struct ContentView: View {
-    @State private var results = [Result]()
+    
     
     var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+        VStack {
+            ScrollView {
+                // Method 001
+                AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 3)
+                
+                
+                // Method 002
+                AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"))
+                { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                
+
+                // Method 003
+                AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                        
+                    } else if phase.error != nil {
+                        Text("There's some error loading the image")
+                    } else {
+                        ProgressView()
+                    }
+                }
             }
-        }
-        .task {
-            await loadData()
         }
     }
 
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid URL")
-        }
-    }
 }
 
 //
