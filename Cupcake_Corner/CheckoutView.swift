208dabeca994ca76c1f8e7d7010c7cb443ade429
Shahid Arshad <shahid.arshad@devsinc.com>
2023-03-02 11:29:42 -0500

    Preparing for checkout

--- Cupcake_Corner/CheckoutView.swift
+++ Cupcake_Corner/CheckoutView.swift
@@ -11,7 +11,25 @@ struct CheckoutView: View {
     @ObservedObject var order: Order
     
     var body: some View {
-        Text("HEyy")
+        ScrollView{
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) {image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                
+                Text("your total is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                Button("Place Order", action: { })
+                    .padding()
+            }
+        }
+        .navigationTitle("Checkout")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
